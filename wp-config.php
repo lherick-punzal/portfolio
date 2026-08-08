995ffc784768e0b6b5004cecfb5b1df7f442e32d
Yosifer <lherickpunzal@gmail.com>
2026-08-08 22:25:30 +0800

    New updated homepage

--- wp-config.php
+++ wp-config.php
@@ -89,7 +89,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_HOME', 'http://localhost/portfolio');
+define('WP_SITEURL', 'http://localhost/portfolio');
 
 /* That's all, stop editing! Happy publishing. */
 
